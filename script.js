7c9952bc6066814323dee084523196296a20396c
duvancho303 <duvancho303@gmail.com>
2022-03-02 22:08:28 -0700

    adding more code

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const countdown = () => {
     const gap = countDate - now;
 
 const seconds = 1000;
-const minutes = second * 60;
+const minutes = seconds * 60;
 const hour = minutes * 60;
-const day = hours * 24;
+const day = hour * 24;
 
 let timeDays = Math.floor(gap / day);
 let timeHours = Math.floor((gap % day) / hour);
@@ -14,6 +14,15 @@ let timeMinutes = Math.floor((gap % hour) / minutes);
 let timeSeconds = Math.floor((gap % minutes) / seconds);
 
 document.querySelector(".day").innerHTML = timeDays;
+document.querySelector(".hour").innerHTML = timeHours;
+document.querySelector(".minutes").innerHTML = timeMinutes;
+document.querySelector(".seconds").innerHTML = timeSeconds;
 
 };
+
+countdown();
+
+
+
+setInterval(countdown, 1000)
 
